7111e38e2ac5ab88d297f773f8d137c1a845d71a
Egor <73159143+egrasimus@users.noreply.github.com>
2021-11-16 15:17:00 +0300

    merged with local

---
+++ js/script.js
@@ -0,0 +1,10 @@
+        document.addEventListener('DOMContentLoaded', function () {
+            var pass1 = document.querySelector('#POST-password')
+                , pass2 = document.querySelector('#POST-password-repeat')
+            pass1.addEventListener('input', function () {
+                this.value != pass2.value ? pass2.setCustomValidity('Пароли не совпадают!') : pass2.setCustomValidity('')
+            })
+            pass2.addEventListener('input', function (e) {
+                this.value != pass1.value ? this.setCustomValidity('Пароли не совпадают!') : this.setCustomValidity('')
+            })
+        })
